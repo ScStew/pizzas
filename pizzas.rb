276493236200ef2660aa6f560d73af1863ed82a3
ScStew <scottmstewart2@gmail.com>
2017-06-28 10:59:46 -0400

    crust random generated

---
+++ pizzas.rb
@@ -0,0 +1,32 @@
+def crust()
+x = 1 + rand(3)
+if 
+	x == 1
+	x = "thin"
+elsif 
+	x == 2
+	x = "regular"
+else
+	x = "stuffed"
+end
+end
+
+def meats()
+
+	end
+
+def veggies()
+
+	end
+def special()
+
+end
+
+def sauces
+	
+	end
+
+	def pizza
+	
+	end
+p crust
