9b4420bda073cdeb60419b879ffe6dfcf2e5c57c
ScStew <scottmstewart2@gmail.com>
2017-07-02 19:17:37 -0400

    pricing started

--- pizzas.rb
+++ pizzas.rb
@@ -1,16 +1,17 @@
+
 def size()
 	s = 1 + rand(3)
 	if 
 		s == 1
 		s = "small"
-		base = 6.00
+		@price = 6
 	elsif 
 		s == 2
 		s = "medium"
-		base = 9.00
+		@price = 9
 	else 
 		s = "large"
-		base = 12.00
+		@price = 12
 	end
 	puts s
 end
@@ -20,47 +21,57 @@ def crust() # thin, regular, stuffed
 		if 
 			x == 1
 			x = "thin"
+		    @p1 = 1
 		elsif 
 			x == 2
 			x = "regular"
+			@p1 = 0
 		else
 			x = "stuffed"
+			@p1 = 2
 		end
 	puts x
 end
 
 def meats() # sausage, pepperoni, hamburger
-	meats = []
+	sausage?()
+	pepperoni?()
+	hamburger?()
+	end
+	
+	@p2 = 0
 
-	def sausage?
-
+	def sausage?()
 		if 1 == [1,2].sample
 			print "sausage\n"
+			@p2 =+ 1
 		end
 	end
-	meats << sausage?
-	def pepperoni?
+
+	def pepperoni?()
 		if 1 == [1,2].sample
 	 		print "pepperoni\n"
+	 		@p2 =+ 0.50
 		end
 	end
-	meats << pepperoni?
-	
-	def hamburger?
+		
+	def hamburger?()
 		if 1 == [1,2].sample
 			print "hamburger\n"
+			@p2 =+ 1
 		end
 	end
-	
-	meats << hamburger?
-end
 
 def veggies()# green peppers, bannana peppers, mushrooms
+	
 	veggies = []
 	
+	@p3 = 0
+
 	def g_peppers?	
 		if 1 == [1,2].sample
 			puts "green peppers"
+			@p3 =+ 0.50
 		end
 	end
 	veggies << g_peppers?
@@ -68,6 +79,7 @@ def veggies()# green peppers, bannana peppers, mushrooms
 	def b_peppers?
 		if 1 == [1,2].sample
 			puts "bannana peppers"
+			@p3 =+ 0.75
 		end
 	end
 	veggies << b_peppers?
@@ -75,6 +87,7 @@ def veggies()# green peppers, bannana peppers, mushrooms
 	def mushrooms?
 		if 1 == [1,2].sample
 			puts "mushrooms"
+			@p3 =+ 1
 		end
 	end
 	
@@ -82,40 +95,52 @@ def veggies()# green peppers, bannana peppers, mushrooms
 end
 
 def sauces() # sauce, no sauce
+
 	y =  1 + rand(3)
 	if 
 		y == 1
 		y = "normal"
+		@p4 = 0
 	elsif 
 		y == 2
 		y = "bbq"
+		@p4 = 1.50
 	else
 		y = "alfredo"
+		@p4 = 1.25
 	end
+	puts y
 end
 def special() #extra sauce, extra cheese, extra toppings
- special = []
- 	def extra_sauce
+ 	
+	@p5 = 0
+
+ 	special = []
+
+ 	def extra_sauce?
 		if 1 == [1,2,3].sample
  			puts "extra sauce"
+ 			@p5 =+ 1.25
  		end
 	end
-special << extra_sauce
+special << extra_sauce?
 
-	def extra_cheese
+	def extra_cheese?
 		if 1 == [1,2,3].sample
 			puts "extra cheese"
+			@p5 =+ 1.50
 		end
 	end
 
-special << extra_cheese
+special << extra_cheese?
 
-	def extra_toppings
+	def extra_toppings?
 		if 1 == [1,2,3].sample
 			puts "extra toppings"
+			@p5 =+ 1.75
 		end
 	end
-	special << extra_toppings
+	special << extra_toppings?
 
 end
 
@@ -128,10 +153,16 @@ def pizza # size, crust, meats, veggies, sauces, special
 	special
 end
 
+def price
+p @price + @p1 + @p2 + @p3 + @p4 + @p5
+end
 def order
 	puts "How many would you like?"
 	(gets.chomp.to_i).times do 
 		pizza
+		
+		price
+
 		puts ""
 	
 	end
